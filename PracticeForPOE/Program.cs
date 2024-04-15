eb6dc716eab7606e23e69ff2588562372b37f1cf
Cade Gamble <ST10262209@vcconnect.edu.za>
2024-04-15 22:46:54 +0200

    Second Commit
    Test to see if it updates

--- PracticeForPOE/Program.cs
+++ PracticeForPOE/Program.cs
@@ -4,3 +4,6 @@ Console.WriteLine("Hello, World!");
 
 //practice for practice POE
 Console.WriteLine("This is a practice");
+
+//next test
+Console.WriteLine("This is the esecond test to see if it updates");
